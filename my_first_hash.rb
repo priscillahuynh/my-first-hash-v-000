c7c78ba9ef88c5cd04a6350fcf9bb43ecf2b34a2
Priscilla H <priscillamhuynh@gmail.com>
2018-06-25 14:28:38 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  my_hash={"fruit"=>"apple","orange", "veggie"=> "broccoli","carrot"}# use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
 end
