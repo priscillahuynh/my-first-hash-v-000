c5e4cd2a07ae86e249815adc8f230b85e93f9b71
Priscilla H <priscillamhuynh@gmail.com>
2018-06-25 14:53:13 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,7 +30,7 @@ shipping_manifest=["muskets"]
 
   # add 4 gun powder to the shipping_manifest hash below
 
-shipping_manifest["gun powder"]=4
+shipping_manifest["gun powder"]
 
   # return the shipping_manifest hash below
 shipping_manifest
